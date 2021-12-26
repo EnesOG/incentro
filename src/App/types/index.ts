2ed05b3176feac0a9ffa5ead89f0708378785744
Enes Sagir <31651856+EnesOG@users.noreply.github.com>
2021-12-26 10:45:08 +0100

    Added create form and incentronaut type

---
+++ src/App/types/index.ts
@@ -0,0 +1,9 @@
+export interface Incentronaut {
+  initials: string;
+  middle_name?: string;
+  last_name: string;
+  postal_code: string;
+  street_name:string;
+  city: string;
+  email: string
+}
